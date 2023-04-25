4cbaea2e3294282809d5a6b6d2bd15089b4d06ee
prabhanjansp <puranikchetan97@gmail.com>
2023-04-25 19:07:28 +0530

    Linked-In Redux state-management

---
+++ src/components/Home.jsx
@@ -0,0 +1,120 @@
+//-------------------------------------async
+import { useDispatch, useSelector } from "react-redux";
+import cartSlice from "../data/cartSlice";
+
+import { fetchAllProducts } from "../data/productSlice";
+import { useEffect } from "react";
+
+const Home = () => {
+  const state = useSelector((state) => state);
+  const { cart, products } = state;
+  const { addToCart, removeFromCart } = cartSlice.actions;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAllProducts("http://localhost:3500/products"));
+  }, [dispatch]);
+
+  return (
+    <div className="container product-catalogue">
+      <div className="row">
+        {products.data?.map((product) => {
+          return (
+            <div className="wrapper col-md-4" key={product.id}>
+              <div className="card">
+                <img
+                  className="card-img-top center-block"
+                  src={product.imageUrl}
+                  alt="Card cap"
+                  height={200}
+                  width={200}
+                />
+
+                <div className="card-body text-center">
+                  <h5 className="card-title">{product.name}</h5>
+                  <p className="card-text">${product.price}</p>
+
+                  {!cart.cartProductIds.includes(product.id) && (
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => dispatch(addToCart(product.id))}
+                    >
+                      Add to cart
+                    </button>
+                  )}
+                  {cart.cartProductIds.includes(product.id) && (
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => dispatch(removeFromCart(product.id))}
+                    >
+                      Remove from cart
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+
+// --------------------------------------sync----------------------------------
+
+// import { useDispatch, useSelector } from "react-redux";
+// import cartSlice from "../data/cartSlice";
+// import productList from "../data/productList.json";
+
+// const Home = () => {
+//   const { cartProductIds } = useSelector((state) => state.cart);
+//   const { addToCart, removeFromCart } = cartSlice.actions;
+//   const dispatch = useDispatch();
+//   console.log(cartProductIds);
+//   return (
+//     <div className="container product-catalogue">
+//       <div className="row">
+//         {productList.products.map((product) => {
+//           return (
+//             <div className="wrapper col-md-4" key={product.id}>
+//               <div className="card">
+//                 <img
+//                   className="card-img-top center-block"
+//                   src={product.imageUrl}
+//                   alt="Card cap"
+//                   height={200}
+//                   width={200}
+//                 />
+
+//                 <div className="card-body text-center">
+//                   <h5 className="card-title">{product.name}</h5>
+//                   <p className="card-text">${product.price}</p>
+
+//                   {!cartProductIds.includes(product.id) && (
+//                     <button
+//                       className="btn btn-primary"
+//                       onClick={() => dispatch(addToCart(product.id))}
+//                     >
+//                       Add to cart
+//                     </button>
+//                   )}
+//                   {cartProductIds.includes(product.id) && (
+//                     <button
+//                       className="btn btn-primary"
+//                       onClick={() => dispatch(removeFromCart(product.id))}
+//                     >
+//                       Remove from cart
+//                     </button>
+//                   )}
+//                 </div>
+//               </div>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
